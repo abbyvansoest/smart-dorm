ced6ba48862165c46a823904eff354aa2ac061de
Abby Van Soest <abbyvansoest@nat-oitwireless-inside-vapornet100-d-24866.Princeton.EDU>
2016-11-27 21:51:05 -0500

    make some charts to save

--- breakdown/make_chart.py
+++ breakdown/make_chart.py
@@ -3,6 +3,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# sum each set of 3 lists in the set of lists
+def sum_day_lists(lists):
+
+	length = len(lists)
+
+	# num = 1
+	# for l in lists:
+	# 	print num
+	# 	print l 
+	# 	print "\n" 
+	# 	num = num + 1
+
+	track_index = 0
+	list1 = lists[0]
+	list2 = lists[1]
+	list3 = lists[2]
+	return_lists = []
+
+	while (track_index < length):
+		add_list = []
+		index = 0
+		while (index < 24):
+			add = list1[index] + list2[index] + list3[index]
+			add_list.append(add)
+			index = index + 1
+		return_lists.append(add_list)
+
+		track_index = track_index + 3
+		if (track_index >= length): 
+			break
+		list1 = lists[track_index]
+		list2 = lists[track_index + 1]
+		list3 = lists[track_index + 2]
+
+		# print track_index
+		# print add_list
+		# print "\n"
+
+	return return_lists
+
 def check_input(days, floor):
 	error = False
 	if (floor != "first" and floor != "second" and floor != "third"):
@@ -24,6 +64,7 @@ def check_input(days, floor):
 	return
 
 mydir = "freq_by_hour/"
+dest_dir = "charts/"
 
 day_input = raw_input("What days would you like to see? (enter space delimited)\n")
 if (day_input == "all"):
@@ -32,10 +73,14 @@ if (day_input == "weekend"):
 	day_input = "saturday sunday"
 if (day_input == "weekday"):
 	day_input = "monday tuesday weds thurs friday"
-floor = raw_input("What floor would you like to see? (choose 1: first, second, or third)\n")
+floor_input = raw_input("What floor would you like to see? (first, second, third, or all)\n")
+floors = []
+if (floor_input == "all"):
+	floors = ["first", "second", "third"]
 
 days = day_input.split(" ")
-check_input(days, floor)
+if not floors:
+	check_input(days, floor_input)
 
 # potential lists to graph
 lists = []
@@ -51,17 +96,37 @@ fig, ax = plt.subplots()
 # for each desired day/floor, open the file for that combo
 # and create a list of the 24 entries
 # add to global list
-for day in days:
-	if day == "":
-		continue
-	day_list = []
+if floors:
+	for day in days:
+		for floor in floors:
+			if day == "":
+				continue
+			day_list = []
 
-	fname = day+"_"+floor+".txt"
-	fo = open(mydir+fname, "rw+")
+			fname = day+"_"+floor+".txt"
+			fo = open(mydir+fname, "rw+")
 
-	for line in fo:
-		day_list.append(int(line))
-	lists.append(day_list)
+			for line in fo:
+				day_list.append(int(line))
+			lists.append(day_list)
+
+elif not floors:
+	for day in days:
+		if day == "":
+			continue
+		day_list = []
+
+		fname = day+"_"+floor+".txt"
+		fo = open(mydir+fname, "rw+")
+
+		for line in fo:
+			day_list.append(int(line))
+		lists.append(day_list)
+
+
+if floors:
+	add_lists = sum_day_lists(lists)
+	lists = add_lists
 
 # Add each day's frequency data to the chart
 offset = 0
@@ -70,6 +135,7 @@ legend_rects = []
 for l in lists:
 
 	#print data
+	print index
 	print '\n'+days[index] 
 	for x in l:
 		print str(x)
@@ -93,13 +159,18 @@ for l in lists:
 	offset = offset + width
 	index = index + 1
 
-ax.legend(legend_rects, days)
+#ax.legend(legend_rects, days)
 
 ax.set_ylabel('Activation Frequency')
 ax.set_xlabel('Hour of the Day (0 is midnight)')
-ax.set_title(floor.capitalize() + " Floor Hour-By-Hour")
+if not floors:
+	ax.set_title(floor.capitalize() + " Floor Hour-By-Hour")
+if floors:
+	ax.set_title("All Floor Hour-By-Hour")
 ax.set_xticks(ind+width)
 ax.set_xticklabels(["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"], horizontalalignment='center')
 
+dest_file = dest_dir + "sun_tues_all_freq.png"
+plt.savefig(dest_file)
 plt.show()
 
